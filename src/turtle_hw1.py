462c1ddbd7228ecf60678cede98f5cf9b7a9dae4
Ritwik Ummalaneni <ritwik1993@gmail.com>
2014-10-17 18:55:10 -0500

    working model - angular velocities not accurate

--- src/turtle_hw1.py
+++ src/turtle_hw1.py
@@ -1,6 +1,33 @@
 #!/usr/bin/env python
 
 import rospy
-import numpy
+import numpy as np
 import math
 import sys
+from geometry_msgs.msg import Twist
+from turtlesim.srv import TeleportAbsolute 
+
+def trajectory_driver(T):
+    twist=Twist()
+    pub=rospy.Publisher('turtle1/cmd_vel',Twist, queue_size=10)
+    rospy.init_node('turtlecmd_tracker',anonymous=True)
+    r=rospy.Rate(60)
+    while not rospy.is_shutdown():
+        t=rospy.get_time()
+        vx=(12*math.pi*math.cos(4*math.pi*t/T))/T
+        vy=(6*math.pi*math.cos(2*math.pi*t/T))/T
+        ax=-48*pow(math.pi,2)*math.sin(4*math.pi*t/T)/pow(T,2)
+        ay=-12*pow(math.pi,2)*math.sin(2*math.pi*t/T)/pow(T,2)
+        twist.linear.x=math.sqrt(pow(vx,2)+pow(vy,2))
+        twist.angular.z=((vx*ay)-(vy*ax))/(pow(vx,2)+pow(vy,2))
+        pub.publish(twist)
+        r.sleep()
+       
+
+if __name__ == '__main__':
+    try:
+        rospy.wait_for_service('turtle1/teleport_absolute')
+        turtle_initial=rospy.ServiceProxy('turtle1/teleport_absolute', TeleportAbsolute)
+        turtle_initial(2.5,2.5,0.785)
+        trajectory_driver(5)
+    except rospy.ROSInterruptException: pass
